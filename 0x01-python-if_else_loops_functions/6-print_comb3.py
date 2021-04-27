ca3702678a40498d464b9d64a3408028d5f34f43
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-04-27 14:28:33 +0300

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,4 +3,4 @@ for num in range(0, 9):
     for num2 in range(num + 1, 10):
         if num != 8 or num2 != 9:
             print("{:n}{:n}".format(num, num2), end=", ")
-print("{:n}{:n}\n".format(num, num2))
+print("{:n}{:n}".format(num, num2))

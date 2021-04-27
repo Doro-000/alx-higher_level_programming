09aa88e0b7c88c0b2171366e4bcd9b5336c5ad99
Doro-000 <hizkyastekle@gmail.com>
2021-04-27 10:40:23 +0000

    more solutions

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -6,7 +6,3 @@ def uppercase(str):
         char = (ord(char) - 32) if 97 <= ord(char) <= 122 else ord(char)
         print("{:c}".format(char), end="")
     print("\n")
-
-if __name__ == "__main__":
-    uppercase("holberton")
-    uppercase("Holberton School 98 Battery street")

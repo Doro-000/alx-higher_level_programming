46e8f79fbd8d21c56f784d469894bfa71b5f525d
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-04-29 19:11:41 +0300

    Update 102-magic_calculations.py

--- 0x02-python-import_modules/102-magic_calculations.py
+++ 0x02-python-import_modules/102-magic_calculations.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-from test import add, sub
 
 
 def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
         return c
-    return sub(a, b)
-
-if __name__ == "__main__":
-    from dis import dis
-    print(dis(magic_calculation))
+    else:
+        return sub(a, b)
+    return None

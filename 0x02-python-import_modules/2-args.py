52eaabbf3e2bbccc51b4d6d79e34a98941a35d9b
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-04-29 19:09:28 +0300

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,12 @@ import sys
 
 if __name__ == "__main__":
     argc = len(sys.argv)
-    print("{:n} {}".format(argc-1, "argument." if argc == 1 else "arguments:"))
+    if argc == 1:
+        arg = "arguments."
+    elif argc == 2:
+        arg = "argument:"
+    else:
+        arg = "arguments:"
+    print("{:n} {}".format(argc-1, arg))
     for arg in range(1, argc):
         print("{:n}: {}".format(arg, sys.argv[arg]))

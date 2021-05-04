9585773f4eb4dcc2499ba1a9d4f2106ef9e3461d
Doro-000 <hizkyastekle@gmail.com>
2021-05-04 16:42:04 +0000

    more sultions

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(my_list=[[]]):
+    if not my_list:
+        return None
+    for row in my_list:
+        for element in range(len(row)):
+            if element < len(row)-1:
+                print("{:d}".format(row[element]), end=" ")
+            else:
+                print("{:d}".format(row[element]))

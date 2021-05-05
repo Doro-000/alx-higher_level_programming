89151fd23e6b34190cab4faaf9a352d6e65a916b
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-05-05 12:38:12 +0300

    Update 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@
 def print_matrix_integer(my_list=[[]]):
     if not my_list:
         return None
-    if not my_list[0] and len(my_list) == 0:
+    if not my_list[0] and len(my_list) == 1:
         print("")
     for row in my_list:
         for element in range(len(row)):

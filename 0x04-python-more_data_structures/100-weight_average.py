213f0da0a23ce83a40a0980144fc0c45e815f886
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-05-06 21:29:46 +0300

    Update 100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+
+
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    numerator = 0
+    denominator = 0
+    for int_tup in my_list:
+        numerator += int_tup[0] * int_tup[1]
+        denominator += int_tup[1]
+    return numerator / denominator

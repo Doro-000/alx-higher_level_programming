62b25553ce6eece91a5dcc595ef08a99b3cc59a1
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-05-18 21:20:35 +0300

    Update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -39,7 +39,7 @@ class Square():
         elif (type(value[0]) is not int) or (type(value[1]) is not int):
             raise TypeError("position must be a tuple of 2 positive integers")
         elif (value[0] < 0) or (value[1] < 0):
-            raise TypeError("position most be a tuple of 2 positive integers")
+            raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def area(self):

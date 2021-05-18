b6669e01cc1cd74df88aa0666bc9dad9e7fccb4c
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-05-18 16:45:38 +0300

    Update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -6,19 +6,23 @@ class Square():
     """square class with it's size and proper validation"""
 
     def __init__(self, size=0, position=(0, 0)):
+        """"Initialize data"""
         self.size = size
         self.position = position
 
     @property
     def size(self):
+        """"get size"""
         return self.__size
 
     @property
     def position(self):
+        """"get position"""
         return self.__position
 
     @size.setter
     def size(self, value):
+        """"set size"""
         if (type(value) is not int):
             raise TypeError("size must be an integer")
         elif (value < 0):
@@ -27,20 +31,23 @@ class Square():
 
     @position.setter
     def position(self, value):
+        """"set position"""
         if (type(value) is not tuple):
             raise TypeError("position must be a tuple of 2 positive integers")
         elif (len(value) != 2):
             raise TypeError("position must be a tuple of 2 positive integers")
-        elif (type(value[0]) is not int) or (value[0] < 0):
+        elif (type(value[0]) is not int) or (type(value[1]) is not int):
             raise TypeError("position must be a tuple of 2 positive integers")
-        elif (type(value[1]) is not int) or (value[1] < 0):
+        elif (value[0] < 0) or (value[1] < 0):
             raise TypeError("position most be a tuple of 2 positive integers")
         self.__position = value
 
     def area(self):
+        """"get area of the square"""
         return self.size ** 2
 
     def my_print(self):
+        """print the square"""
         if not self.size:
             print("")
         for i in range(self.size):

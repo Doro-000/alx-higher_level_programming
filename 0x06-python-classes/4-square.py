c7440016e2ec517fcd4d9cc1ebeff7785a02abe4
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-05-18 16:32:08 +0300

    Update 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -5,12 +5,12 @@
 class Square():
     """square class with it's size and proper validation"""
 
-    def __init__(self, size):
+    def __init__(self, size=0):
         self.__size = size
 
     @property
     def size(self):
-        return self._size
+        return self.__size
 
     @size.setter
     def size(self, value):

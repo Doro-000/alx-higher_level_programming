34eddd6ce284285118759a89b6c3fd993ed6dae1
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-05-20 15:33:33 +0300

    Update 2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -8,8 +8,10 @@ def matrix_divided(matrix, div):
                  @matrix: matrix to be divided
                  @div: dividend
     """
-    if div == 0:
+    if type(div) not in [int, float]:
         raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
     if len(matrix) == 0:
         return
     new_matrix = []

8e60ea32f7c5ace68cccbc766b1b36593eb3ea47
Doro-000 <hizkyastekle@gmail.com>
2021-05-20 08:39:06 +0000

    fixing pep issues

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -7,14 +7,15 @@ class Node():
     def __init__(self, data, next_node=None):
         self.data = data
         self.next_node = next_node
-    
+
     @property
     def data(self):
         return self.__data
+
     @property
     def next_node(self):
         return self.__next_node
-    
+
     @data.setter
     def data(self, value):
         if (type(value) is not int):
@@ -23,7 +24,7 @@ class Node():
 
     @next_node.setter
     def next_node(self, value):
-        if (type(value) == Node) or (value == None):
+        if (type(value) == Node) or (value is None):
             self.__next_node = value
         else:
             raise TypeError("next_node must be a Node object")
@@ -33,6 +34,7 @@ class SinglyLinkedList():
     """Class to implement a singly linked list"""
     def __init__(self):
         self.__head = None
+
     def sorted_insert(self, value):
         new_node = Node(value, None)
         if (not self.__head):
@@ -41,16 +43,17 @@ class SinglyLinkedList():
             new_node.next_node = self.__head
             self.__head = new_node
         else:
-            largest = self.__head
-            while (largest.next_node != None and largest.next_node.data < value):
-                largest = largest.next_node
-            new_node.next_node = largest.next_node
-            largest.next_node = new_node
+            temp = self.__head
+            while (temp.next_node is not None and temp.next_node.data < value):
+                temp = temp.next_node
+            new_node.next_node = temp.next_node
+            temp.next_node = new_node
+
     def __str__(self):
         result = []
         string = ""
         largest = self.__head
-        while (largest != None):
+        while (largest is not None):
             result.append(str(largest.data))
             largest = largest.next_node
         return "\n".join(result)

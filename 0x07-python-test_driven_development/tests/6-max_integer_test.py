f2869c984d24fa3b808acb8bca751c437b53041d
Doro-000 <hizkyastekle@gmail.com>
2021-05-21 19:23:21 +0000

    testing

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -6,14 +6,17 @@ import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 
-class TestMaxInteger(unitest.TestCase):
+class TestMaxInteger(unittest.TestCase):
     """test class for max_integer"""
     def test_ProperInput(self):
+        """test proper input"""
         self.assertEqual(max_integer([1, 2, 3]), 3)
         self.assertEqual(max_integer([1]), 1)
         self.assertEqual(max_integer(['a', 'b', 'c']), 'c')
         self.assertEqual(max_integer([2.0, 2.1, 2.3]), 2.3)
+
     def test_EmptyInput(self):
+        """test empty input"""
         self.assertEqual(max_integer([]), None)
 
 if __name__ == "__main__":

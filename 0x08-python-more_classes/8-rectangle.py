e83bcd5b640300919992c7143e9a3fcad4b4efd1
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-05-24 19:19:47 +0300

    Update 8-rectangle.py

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -9,11 +9,11 @@ class Rectangle():
     print_symbol = '#'
 
     def __init__(self, width=0, height=0):
-        if type(width) is not str:
+        if type(width) is not int:
             raise TypeError("width must be an integer")
         if width < 0:
             raise ValueError("width must be >= 0")
-        if type(height) is not str:
+        if type(height) is not int:
             raise TypeError("height must be an integer")
         if height < 0:
             raise ValueError("height must be >= 0")
@@ -31,7 +31,7 @@ class Rectangle():
 
     @width.setter
     def width(self, value):
-        if type(value) is not str:
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -39,7 +39,7 @@ class Rectangle():
 
     @height.setter
     def height(self, value):
-        if type(value) is not str:
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")

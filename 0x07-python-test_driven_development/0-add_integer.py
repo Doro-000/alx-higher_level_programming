5d230e6998ca7e067b9ab6eb554bff031989caea
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-05-26 13:01:29 +0300

    Update 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -8,7 +8,7 @@ def add_integer(a, b=98):
                  @a: first integer
                  @b: second integer, defaults to 98 if not given
     """
-    if a is None or type(a) not in [int, float]:
+    if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
         raise TypeError("b must be an integer")

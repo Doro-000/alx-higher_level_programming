169fea72d9f3707d567523eea75f910420921ca2
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-05-31 20:00:53 +0300

    Update 3-is_kind_of_class.py

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """contains the function is_kind_of_class"""
 
+
 def is_kind_of_class(obj, a_class):
     """decides if obj is an instance or subclass of a_class"""
     return isinstance(obj, a_class) or issubclass(obj, a_class)

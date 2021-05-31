552d89deb063fc837bace9ee5a372d036906ee3c
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-05-31 20:01:33 +0300

    Update 4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """contains the function inherits_from"""
 
+
 def inherits_from(obj, a_class):
     """decides if obj is a subclass of a_class"""
     return issubclass(obj, a_class)

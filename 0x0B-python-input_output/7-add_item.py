281725f7db2eafca2cb1a5d3ed585ecacb64ff3d
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-06-01 18:40:56 +0300

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,17 +2,14 @@
 import sys
 
 
-def load_from_json_file(filename):
-    """creates an Object from a JSON file"""
-    with open(filename, "r") as f:
-        return json.load(f)
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-def save_to_json_file(my_obj, filename):
-    """writes an Object to a text file, using a JSON"""
-    with open(filename, "w") as f:
-        json.dump(my_obj, f)
-
-my_list = load_from_json_file("add_item.json")
-for i in range(1, len(sys.argv)):
-    my_list.append(sys.argv[i])
-save_to_list_file(my_list, add_item.json)
+try:
+    my_list = load_from_json_file("add_item.json")
+    for i in range(1, len(sys.argv)):
+        my_list.append(sys.argv[i])
+    save_to_json_file(my_list, "add_item.json")
+except FileNotFoundError:
+    with open("add_item.json", "w") as f:
+        json.dump([], f)

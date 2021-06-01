6057a8078ca8812c9e02951b65266c021a93a037
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-06-01 20:29:08 +0300

    Update 100-append_after.py

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""contains append_after"""
 
 
 def append_after(filename="", search_string="", new_string=""):
+    """ inserts a line of text to a file"""
     with open(filename, "r+") as f:
         content = f.readlines()
         for i in range(len(content)):

4a7e281f42c26ff705d9db22e528ff43c7a3601f
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-06-01 08:29:28 +0300

    Update 9-rectangle.py

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -2,19 +2,7 @@
 """contains classes to represent different shapes"""
 
 
-class BaseGeometry():
-    """BaseGeometry class"""
-    def area(self):
-        """raises an exception"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """validates an int"""
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """Rectangle class to represent rectangles"""

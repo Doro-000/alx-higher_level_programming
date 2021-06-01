42aa459cfd05a69394d6aa72027315330d3bf7ee
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-06-01 18:14:24 +0300

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,8 +2,15 @@
 import sys
 
 
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+def load_from_json_file(filename):
+    """creates an Object from a JSON file"""
+    with open(filename, "r") as f:
+        return json.load(f)
+
+def save_to_json_file(my_obj, filename):
+    """writes an Object to a text file, using a JSON"""
+    with open(filename, "w") as f:
+        json.dump(my_obj, f)
 
 my_list = load_from_json_file("add_item.json")
 for i in range(1, len(sys.argv)):

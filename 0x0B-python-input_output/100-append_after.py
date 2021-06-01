c1ef9eb4b1bcad3918e36a2e858c6c795b4a4e26
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-06-01 20:59:06 +0300

    Update 100-append_after.py

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -6,9 +6,9 @@ def append_after(filename="", search_string="", new_string=""):
     """ inserts a line of text to a file"""
     with open(filename, "r+", encoding="utf-8") as f:
         content = f.readlines()
-        for i in range(len(content)):
-            if search_string in content[i]:
-                content.insert(i + 1, new_string)
         f.seek(0)
         f.truncate(0)
-        f.writelines(content)
+        for i in range(len(content)):
+            f.write(content[i])
+            if search_string in content[i]:
+                f.write(new_string)

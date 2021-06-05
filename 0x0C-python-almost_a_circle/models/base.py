819b6610bf0ea588ad1a101b1834e6cfb99f0efa
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-06-05 02:21:26 -0700

    Add files via upload

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""Contains Base class"""
+import json
+
+
+class Base:
+    """base class to manage ID
+    Attributes:
+            id: int, Id of a Base object
+    """
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """instantiation"""
+        if id:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """serializes a list of dicts representing shapes"""
+        if list_dictionaries == None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """deserializes a json list represented as a string"""
+        if json_string == None or json_string == "" or json_string == "[]":
+            return []
+        return json.loads(json_string)
+
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """saves a list of objects to a json file"""
+        filename = "{}.json".format(cls.__name__)
+        with open(filename, "w") as f:
+            f.write(cls.to_json_string([o.to_dictionary() for o in list_objs]))
+
+    @classmethod
+    def create(cls, **dictionary):
+        """creates an instances with all attributes set"""
+        new = cls.__new__(cls)
+        new.__init__(10, 10)
+        new.update(**dictionary)
+        return new
+
+    @classmethod
+    def load_from_file(cls):
+        """retrives a list of instances from a json file"""
+        filename = "{}.json".format(cls.__name__)
+        instances = []
+        try:
+            with open(filename, "r") as f:
+                content = cls.from_json_string(f.read())
+            for dict in content:
+                instances.append(cls.create(**dict))
+            return instances
+        except FileNotFoundError:
+            return instances
---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+from rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """class to represent a Square"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """instantiation"""
+        super().__init__(size, size, x, y, id)
+        self.size = size
+
+    @property
+    def size(self):
+        """size attribute getter"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """size attribute setter"""
+        self.width = value
+        self.height = value
+        self.__size = value
+
+    def __str__(self):
+        """formatted output"""
+        s = "[Square] ({}) {}/{} - {}".format(self.id, self.x,
+                                            self.y, self.width)
+        return s
+
+    def update(self, *args, **kwargs):
+        """updates a square with new values for attributes"""
+        attrs = ["id", "size", "x", "y"]
+        if args != None and len(args) != 0:
+            i = 0
+            while i < len(attrs) and i < len(args):
+                setattr(self, attrs[i], args[i])
+                i += 1
+        else:
+            for attr in attrs:
+                if attr in kwargs.keys():
+                    setattr(self, attr, kwargs[attr])
+
+    def to_dictionary(self):
+        """represents a Square as a dict"""
+        rect_dict = {}
+        attrs = ["id", "size", "x", "y"]
+        for attr in attrs:
+            rect_dict.update({attr:getattr(self, attr)})
+        return rect_dict

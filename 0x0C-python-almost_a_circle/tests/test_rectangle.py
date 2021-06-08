fad9ae5f779ecb2ad4bd71a6ae7e70d49b28c16c
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-06-08 11:36:53 +0300

    more tests

--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 import unittest
+import io
+import sys
 from models.rectangle import Rectangle
 from models.base import Base
 
@@ -20,7 +22,7 @@ class TestRectCls(unittest.TestCase):
         self.assertEqual(x.y, 10)
         self.assertEqual(x.id, 15)
         
-    def test_attrs_validation(self):
+     def test_attrs_validation(self):
         Base._Base__nb_objects = 0
         with self.assertRaisesRegex(TypeError, "width must be an integer"):
             y = Rectangle("20", 20)
@@ -30,6 +32,69 @@ class TestRectCls(unittest.TestCase):
             y = Rectangle(20, 20, "x", 10)
         with self.assertRaisesRegex(TypeError, "y must be an integer"):
             y = Rectangle(20, 20, 10, "y")
+        with self.assertRaises(TypeError):
+            y = Rectangle(float("NaN"), float("inf"))
+            y = Rectangle(float("inf"), float("NaN"))
+            y = Rectangle(float("NaN"), float("NaN"), float("NaN"), float("NaN"))
+            y = Rectangle(float("inf"), float("inf"), float("inf"), float("inf"))
+            y = Rectangle(None, None)
+            y = Rectangle()
+            y = Rectangle([10, 10], [10, 10], [10, 10], [10, 10])
+            y= Rectangle("abebe", "abebe", "abebe", "abebe")
+        with self.assertRaisesRegex(ValueError, "width must be > 0"):
+            y = Rectangle(-20, 20)
+            y = Rectangle(0, 20)
+            y = Rectangle(-20000000, 20)
+        with self.assertRaisesRegex(ValueError, "height must be > 0"):
+            y = Rectangle(20, -20)
+            y = Rectangle(20, 0)
+            y = Rectangle(20, -20000000)
+        with self.assertRaisesRegex(ValueError, "x must be >= 0"):
+            y = Rectangle(10, 10, -10)
+            y = Rectangle(10, 10, -100000)
+        with self.assertRaisesRegex(ValueError, "y must be >= 0"):
+            y = Rectangle(10, 10, 10, -10)
+            y = Rectangle(10, 10, 10, -100000)
+    def test_area(self):
+        Base._Base__nb_objects = 0
+        x = Rectangle(10, 10)
+        self.assertEqual(x.area(), 100)
+        x = Rectangle(1, 1234)
+        self.assertEqual(x.area(), 1234)
+        x = Rectangle(1345, 1)
+        self.assertEqual(x.area(), 1345)
+
+    def test_display(self):
+        Base._Base__nb_objects = 0
+
+        captured_print = io.StringIO()
+        sys.stdout = captured_print
+
+        x = Rectangle(2, 2, 2, 2)
+        x.display()
+        self.assertEqual(captured_print.getvalue(), "\n\n  ##\n  ##\n")
+        captured_print.seek(0)
+        captured_print.truncate(0)
+
+        x = Rectangle(1, 1)
+        x.display()
+        self.assertEqual(captured_print.getvalue(), "#\n")
+        captured_print.seek(0)
+        captured_print.truncate(0)
+
+        x = Rectangle(1, 1)
+        x.display()
+        self.assertEqual(captured_print.getvalue(), "#\n")
+        captured_print.seek(0)
+        captured_print.truncate(0)
+
+        x = Rectangle(1, 1, 2, 2)
+        x.display()
+        self.assertEqual(captured_print.getvalue(), "\n\n  #\n")
+        captured_print.seek(0)
+        captured_print.truncate(0)
+
+        sys.stdout = sys.__stdout__
 
 if __name__ == "__main__":
     unittest.main()

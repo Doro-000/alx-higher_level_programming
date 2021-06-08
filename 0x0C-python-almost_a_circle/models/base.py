2e3a113ebc2960ba9fcd0196f8fe93756ca8aba1
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-06-08 14:21:41 +0300

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -24,7 +24,7 @@ class Base:
     def to_json_string(list_dictionaries):
         """serializes a list of dicts representing shapes"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
-            return "[]"
+            return json.dumps([])
         return json.dumps(list_dictionaries)
 
     @staticmethod

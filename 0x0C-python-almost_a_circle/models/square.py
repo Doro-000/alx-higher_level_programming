c287687853830dd5daf746497a5cf75f919968f8
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-06-08 13:43:48 +0300

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,5 +6,11 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """class to represent a Square"""
     def __init__(self, size, x=0, y=0, id=None):
-        """instantiation"""
+        """Instantiation of a Square object
+        Args:
+            size (int): the size of the square
+            x (int): the x coordinate
+            y (int): the y coordinate
+            id (int): the id of the object
+        """
         super().__init__(size, size, x, y, id)

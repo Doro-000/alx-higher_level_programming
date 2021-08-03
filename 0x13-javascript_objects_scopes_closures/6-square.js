c34ff1dd40683c0e12fb147e9366933852ac619a
Hizkyas Tekle <hizkyastekle@gmail.com>
2021-08-03 20:45:28 +0300

    noticed dumb mistake

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,12 +3,11 @@ const ParentSquare = require('./5-Square.js');
 
 class Square extends ParentSquare {
   charPrint (c) {
-    let char = c;
     if (c === undefined) {
-      char = 'X';
+      c = 'X';
     }
-    for (let i = 0; i < this.width; i++) {
-      console.log(char.repeat(this.width));
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
     }
   }
 }

a94c476f1387ec186d17486df04d837eb507563e
Hizkyas Tekle <hizkyastekle@gmail.com>
2021-08-03 20:49:04 +0300

    uffff

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,13 +1,14 @@
 #!/usr/bin/node
-const ParentSquare = require('./5-Square.js');
+const ParentSquare = require('./5-square.js');
 
 class Square extends ParentSquare {
   charPrint (c) {
     if (c === undefined) {
-      c = 'X';
-    }
-    for (let i = 0; i < this.height; i++) {
-      console.log(c.repeat(this.width));
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
     }
   }
 }

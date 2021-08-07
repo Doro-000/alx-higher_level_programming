9a5e737cac58f0decabe74016e2373869ca2c80e
Hizkyas Tekle <hizkyastekle@gmail.com>
2021-08-07 17:18:06 +0300

    advanced tasks

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-"""creates the State "California" with the
-City "San Francisco" from the database hbtn_0e_100_usa"""
+"""lists all State objects, and corresponding City objects,
+contained in the database hbtn_0e_101_usa"""
 
 from relationship_state import Base, State
 from relationship_city import City
@@ -12,14 +12,13 @@ from sys import argv
 if __name__ == "__main__":
     engine = create_engine(
         "mysql+mysqldb://{}:{}@localhost/{}".format(argv[1], argv[2], argv[3]))
-    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
-
     my_session = Session()
-    my_state = State(name="California")
-    my_state.cities = [City(name="San Francisco")]
 
-    my_session.add(my_state)
-    my_session.commit()
+    for state in my_session.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
+        for city in state.cities:
+            print("\t{}: {}".format(city.id, city.name))
+
     my_session.close()
--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
-"""creates the State "California" with the
-City "San Francisco" from the database hbtn_0e_100_usa"""
+"""lists all City objects from the database hbtn_0e_101_usa"""
 
 from relationship_state import Base, State
 from relationship_city import City
@@ -12,14 +11,11 @@ from sys import argv
 if __name__ == "__main__":
     engine = create_engine(
         "mysql+mysqldb://{}:{}@localhost/{}".format(argv[1], argv[2], argv[3]))
-    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
-
     my_session = Session()
-    my_state = State(name="California")
-    my_state.cities = [City(name="San Francisco")]
 
-    my_session.add(my_state)
-    my_session.commit()
+    for city in my_session.query(City).order_by(City.id).all():
+        print("{}: {} -> {}".format(city.id, city.name, city.state.name))
+
     my_session.close()

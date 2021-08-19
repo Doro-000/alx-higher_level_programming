300c84ed2167b144b9dc6bba4ebb4d9f4d00163b
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-08-19 23:00:15 +0300

    just in case

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,9 +8,10 @@ def find_peak(list_of_integers):
             return None
         if (len(list_of_integers) == 1):
             return list_of_integers[0]
-        list_1 = list_of_integers[0: len(list_of_integers) // 2]
+        half = len(list_of_integers) // 2
+        list_1 = list_of_integers[0: half]
         avg_1 = sum(list_1) / len(list_1)
-        list_2 = list_of_integers[len(list_of_integers) // 2:]
+        list_2 = list_of_integers[half:]
         avg_2 = sum(list_2) / len(list_2)
         if (avg_1 > avg_2):
             return find_peak(list_1)

6845562341a5728784efbd86a0f453a782cd26f8
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-08-19 23:50:11 +0300

    just to see, very slow, i think

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,19 +3,23 @@
 
 
 def find_peak(list_of_integers):
-        """finds a peak in a list of unsorted integers"""
-        if (not list_of_integers):
-                return None
-        if (len(list_of_integers) == 1):
-                return list_of_integers[0]
-        half = len(list_of_integers) // 2
-        list_1 = list_of_integers[0: half]
-        avg_1 = sum(list_1) / len(list_1)
-        list_2 = list_of_integers[half:]
-        avg_2 = sum(list_2) / len(list_2)
-        if (avg_1 > avg_2):
-                return find_peak(list_1)
-        elif (avg_1 < avg_2):
-                return find_peak(list_2)
-        else:
-                return max(find_peak(list_1), find_peak(list_2))
+	"""finds a peak in a list of unsorted integers"""
+	if (not list_of_integers):
+		return None
+	if (len(list_of_integers) <= 2):
+		return max(list_of_integers)
+	peak = None
+	if (list_of_integers[0] >= list_of_integers[1]):
+		peak = list_of_integers[0]
+	if (list_of_integers[-1] >= list_of_integers[-2]):
+		peak = list_of_integers[-1]
+	i = 1
+	while (i < len(list_of_integers) - 1):
+		if (list_of_integers[i] >= list_of_integers[i + 1] and list_of_integers[i] >= list_of_integers[i - 1]):
+			if (peak):
+				if (peak < list_of_integers[i]):
+					peak = list_of_integers[i]
+			else:
+				peak = list_of_integers[i]
+			i += 2
+	return peak

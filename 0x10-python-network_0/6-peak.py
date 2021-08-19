61558cfcc944fcefecfeba8b38d544f33303a2ab
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-08-19 22:22:25 +0300

    just to try

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,17 +4,4 @@
 
 def find_peak(list_of_integers):
         """finds a peak in a list of unsorted integers"""
-        if (not list_of_integers):
-            return None
-        if (len(list_of_integers) == 1):
-            return list_of_integers[0]
-        if (len(list_of_integers) == 2):
-            return max(list_of_integers)
-        list_1 = list_of_integers[0: len(list_of_integers) // 2]
-        avg_1 = sum(list_1) / len(list_1)
-        list_2 = list_of_integers[len(list_of_integers) // 2:]
-        avg_2 = sum(list_2) / len(list_2)
-        if (avg_1 > avg_2):
-            return find_peak(list_1)
-        else:
-            return find_peak(list_2)
+        return max(list_of_integers)

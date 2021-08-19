f9d483321b70be5a744dd8d0da802f9f0e98f119
Doro-000 <hizkyastekle@gmail.com>
2021-08-19 21:09:07 +0000

    pep

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,26 +3,26 @@
 
 
 def find_peak(list_of_integers):
-	"""finds a peak in a list of unsorted integers"""
-	if (not list_of_integers):
-		return None
-	if (len(list_of_integers) <= 2):
-		return max(list_of_integers)
-	peak = None
-	if (list_of_integers[0] >= list_of_integers[1]):
-		peak = list_of_integers[0]
-	if (list_of_integers[-1] >= list_of_integers[-2]):
-		peak = list_of_integers[-1]
-	i = 1
-	while (i < len(list_of_integers) - 1):
-		if (list_of_integers[i] >= list_of_integers[i + 1] and\
-		    list_of_integers[i] >= list_of_integers[i - 1]):
-			if (peak):
-				if (peak < list_of_integers[i]):
-					peak = list_of_integers[i]
-			else:
-				peak = list_of_integers[i]
-			i += 2
-		else:
-			i += 1
-	return peak
+    """finds a peak in a list of unsorted integers"""
+    if (not list_of_integers):
+        return None
+    if (len(list_of_integers) <= 2):
+        return max(list_of_integers)
+    peak = None
+    if (list_of_integers[0] >= list_of_integers[1]):
+        peak = list_of_integers[0]
+    if (list_of_integers[-1] >= list_of_integers[-2]):
+        peak = list_of_integers[-1]
+    i = 1
+    while (i < len(list_of_integers) - 1):
+        if (list_of_integers[i] >= list_of_integers[i + 1] and
+                list_of_integers[i] >= list_of_integers[i - 1]):
+            if (peak):
+                if (peak < list_of_integers[i]):
+                    peak = list_of_integers[i]
+            else:
+                peak = list_of_integers[i]
+            i += 2
+        else:
+            i += 1
+    return peak

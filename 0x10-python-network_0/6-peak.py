912fdadaabe426b4166c777eca9106111e42fef4
Doro-000 <hizkyastekle@gmail.com>
2021-08-19 17:19:38 +0000

    pep issues

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,18 +3,18 @@
 
 
 def find_peak(list_of_integers):
-    """finds a peak in a list of unsorted integers"""
-	if (not list_of_integers):
-		return None
-	if (len(list_of_integers) == 1):
-		return list_of_integers[0]
-	list_1 = list_of_integers[0: len(list_of_integers) // 2]
-	avg_1 = sum(list_1) / len(list_1)
-	list_2 = list_of_integers[len(list_of_integers) // 2:]
-	avg_2 = sum(list_2) / len(list_2)
-	if (avg_1 > avg_2 ):
-		return find_peak(list_1)
-	elif (avg_1 < avg_2):
-		return find_peak(list_2)
-	else:
-		return max(find_peak(list_1), find_peak(list_2))
+        """finds a peak in a list of unsorted integers"""
+        if (not list_of_integers):
+            return None
+        if (len(list_of_integers) == 1):
+            return list_of_integers[0]
+        list_1 = list_of_integers[0: len(list_of_integers) // 2]
+        avg_1 = sum(list_1) / len(list_1)
+        list_2 = list_of_integers[len(list_of_integers) // 2:]
+        avg_2 = sum(list_2) / len(list_2)
+        if (avg_1 > avg_2):
+            return find_peak(list_1)
+        elif (avg_1 < avg_2):
+            return find_peak(list_2)
+        else:
+            return max(find_peak(list_1), find_peak(list_2))

4e6886fc8a787422441f00fbe5393e926857ab79
Doro-000 <hizkyastekle@gmail.com>
2021-08-19 12:39:37 +0000

    first question

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,1 +1,6 @@
 #!/usr/bin/env bash
+# takes in a URL, sends a request to that URL,
+# and displays the size of the body of the response
+
+size=$(curl -Is "$1" 2>&1 | grep Content-Length | cut -d' ' -f2)
+echo "$size"

fdc13568945d135245e91f7b438996aa3e4ea064
Doro-000 <hizkyastekle@gmail.com>
2021-08-19 12:45:59 +0000

    debugging and updating sheabang

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# takes in a URL, sends a request to that URL,
-# and displays the size of the body of the response
-
-size=$(curl -Is "$1" 2>&1 | grep Content-Length | cut -d' ' -f2)
-echo "$size"
+# takes in a URL, sends a request to that URL, and displays the size of the body
+echo $(curl -Is "$1" 2>&1 | grep Content-Length | cut -d' ' -f2)

793f494f97f8da70435c43f3c730c4c460fc9333
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-08-20 14:33:05 +0300

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
         response = response.read()
         print("Body response:")
         print("\t - type: {}".format(type(response)))
-        print("\t - content: {}".format(response.decode('Ascii')))
+        print("\t - content: {}".format(response))
         print("\t - utf8 content: {}".format(response.decode('utf-8')))

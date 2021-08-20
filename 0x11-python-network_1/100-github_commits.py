8dd7be3b7a47b8a2f2bc36446f5007e2cfe4ee33
Hizkyas Tekle <hizkyastekle@gmail.com>
2021-08-20 15:02:50 +0300

    advanced task

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""takes a repository name and an owner name and
+lists 10 commits (from the most recent to oldest)"""
+from sys import argv
+from requests import get
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos"
+    owner = argv[2]
+    repo = argv[1]
+    url = url + "/" + owner + "/" + repo + "/" + "commits"
+    limit = {"per_page":10}
+    response = get(url, params=limit)
+    for commmit in response.json():
+        print("{}: {}".format(commmit.get("sha"), commmit.get("author").get("login")))

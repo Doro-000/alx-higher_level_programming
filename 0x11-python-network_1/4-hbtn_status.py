5f1eab820163d3527e7034346a89903d260e4d17
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-08-20 14:31:34 +0300

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """fetches https://intranet.hbtn.io/status using requests"""
 from requests import get
-from sys import argv
 
 if __name__ == "__main__":
-    response = get(argv[1]).text
+    response = get("https://intranet.hbtn.io/status").text
     print("Body response:")
     print("\t- type: {}".format(type(response)))
     print("\t- content: {}".format(response))

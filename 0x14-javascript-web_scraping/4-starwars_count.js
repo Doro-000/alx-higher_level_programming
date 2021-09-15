2c576eb7818d049a60d417e479c8a364f92f7627
Hizkyas Tekle <hizkyastekle@gmail.com>
2021-09-15 05:35:44 +0300

    better solution

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,31 +2,16 @@
 const request = require('request');
 const { argv } = require('process');
 
-let count = 0;
-function MakeRequest (url) {
-  return new Promise(function (resolve, reject) {
-    request(url, (error, response, body) => {
-      if (!error && response.statusCode === 200) {
-        resolve(body);
-      } else {
-        reject(error);
-      }
-    });
-  });
-}
-
-async function main () {
-  let movies = await MakeRequest(argv[2]);
-  movies = JSON.parse(movies).results;
-  movies.forEach(element => {
-    element.characters.forEach(element => {
-      const CharacterId = element.slice(37, -1);
-      if (CharacterId === '18') {
-        count++;
-      }
-    });
-  });
-  console.log(count);
-}
-
-main();
+request(argv[2], (error, response, body) => {
+  if (error) {
+    console.error(error);
+  }
+  let Count = 0;
+  const doc = JSON.parse(body).results;
+  doc.forEach(p => p.characters.forEach(msg => {
+    if (msg.slice(37, -1) === '18') {
+      Count++;
+    }
+  }));
+  console.log(Count);
+});
